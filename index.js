8c647ee722d2f9dde0885dc064bcf2181d3ec20e
DimaPalm <103450430+DimaPalm@users.noreply.github.com>
2022-04-11 20:49:06 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+echo "# CryptaDima" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/DimaPalm/CryptaDima.git
+git push -u origin main
